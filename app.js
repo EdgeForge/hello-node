a82721430bacc84526764d3cbcb683de3ef751ba
James Jones <james@edgeforge.com>
2022-03-10 03:53:10 +0000

    refactor for unit

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
-const http = require('http');
+const http = require('unit-http');
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
+http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
   res.end('Hello from Nodejs');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+}).listen();

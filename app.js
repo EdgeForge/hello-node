30afcb1c5e14a61ea1ba721592ef5c4dd918676e
Vinnie Mazza <vinnie.mazza@gmail.com>
2022-03-14 15:45:23 +0000

    dogtv inital

--- app.js
+++ app.js
@@ -1,14 +1,34 @@
-const http = require('http');
+const http = require('unit-http');
+var giphy = require('giphy-api')();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+async function getDog() {
+  return new Promise((resolve, reject) => {
+    try {
+      giphy.search({
+        q: 'dog',
+        rating: 'g',
+        limit: 50
+      }, function(err, res) {
+        // Res contains gif data!
+        resolve(res)
+      });
+    } catch {
+      resolve(reject)
+    }
+  })
+}
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+http.createServer(async(req, response) => {
+  console.log(req.url)
+  let dog = await getDog()
+  let whichDog = Math.floor(Math.random() * 50)
+  response.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/plain');
+  // response.setHeader('Content-Type', 'image/gif');
+  response.setHeader('Content-Type', 'text/html');
+  // response.end('Hello from Nodejs, where james buys dog tv', dog);
+  // console.log(dog.data[0].embed_url)
+  console.log(dog.data[whichDog].images.original.url) 
+  response.end('<html><p>Hello from Nodejs, where james buys dog tv</p><img src="' + dog.data[whichDog].images.original.url + '" alt="dogtv" width="500" height="600"></html>');
+  // response.end('<img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg">');
+}).listen(3000);

8dfcef36c636d9650d7b5c9725065c6388904dc1
James Jones <james@edgeforge.com>
2022-03-16 21:15:15 +0000

    modified for slack slash command

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const http = require('unit-http');
+const http = require('http');
 var giphy = require('giphy-api')();
 
 async function getDog() {
@@ -23,12 +23,20 @@ http.createServer(async(req, response) => {
   let dog = await getDog()
   let whichDog = Math.floor(Math.random() * 50)
   response.statusCode = 200;
-  // res.setHeader('Content-Type', 'text/plain');
-  // response.setHeader('Content-Type', 'image/gif');
-  response.setHeader('Content-Type', 'text/html');
+  response.setHeader('Content-Type', 'application/json');
   // response.end('Hello from Nodejs, where james buys dog tv', dog);
   // console.log(dog.data[0].embed_url)
-  console.log(dog.data[whichDog].images.original.url) 
-  response.end('<html><p>Hello from Nodejs, where james buys dog tv</p><img src="' + dog.data[whichDog].images.original.url + '" alt="dogtv" width="500" height="600"></html>');
-  // response.end('<img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg">');
+  console.log(dog.data[whichDog].images.original.url)
+  let slackJson = {
+    "blocks": [
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "![DogTV]("+dog.data[whichDog].images.original.url+")"
+			}
+		}
+	]
+}
+  response.end(JSON.stringify(slackJson));
 }).listen(3000);

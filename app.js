0b6691908498c03d12b3928239cbbd142d48ed85
James Jones <james@edgeforge.com>
2022-03-18 18:20:08 +0000

    breed query

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const http = require('unit-http');
+const http = require('http');
 var giphy = require('giphy-api')();
+const url = require('url');
 
-async function getDog() {
+let breeds = [
+  "pug",
+  "poodle",
+  "retriver",
+  "chihuahua",
+  "dobermann",
+  "mutt"
+];
+
+async function getDog(q) {
   return new Promise((resolve, reject) => {
     try {
       giphy.search({
-        q: 'dog',
+        q: q,
         rating: 'g',
         limit: 50
       }, function(err, res) {
@@ -20,15 +30,29 @@ async function getDog() {
 
 http.createServer(async(req, response) => {
   console.log(req.url)
-  let dog = await getDog()
-  let whichDog = Math.floor(Math.random() * 50)
+  let dogQuery = "dog"
+  let dogUrl
+  let breed = url.parse(req.url, true).query.breed;
+  if(breed){
+    if(breeds.includes(breed)){
+      console.log("Looking for Breed: %s", breed)
+      dogQuery += " "+breed
+    } else {
+      console.log("BAD DOG")
+      dogQuery = "BAD DOG"
+    }
+  }
+
+  if(dogQuery == "BAD DOG"){
+    dogUrl = "https://i.giphy.com/media/owRSsSHHoVYFa/giphy.webp"
+  } else {
+    let dog = await getDog(dogQuery)
+    let whichDog = Math.floor(Math.random() * 50)
+    dogUrl = dog.data[whichDog].images.original.url
+  }
+  
   response.statusCode = 200;
-  // res.setHeader('Content-Type', 'text/plain');
-  // response.setHeader('Content-Type', 'image/gif');
   response.setHeader('Content-Type', 'text/html');
-  // response.end('Hello from Nodejs, where james buys dog tv', dog);
-  // console.log(dog.data[0].embed_url)
-  console.log(dog.data[whichDog].images.original.url) 
-  response.end('<html><p>Hello from Nodejs, where james buys dog tv</p><img src="' + dog.data[whichDog].images.original.url + '" alt="dogtv" width="500" height="600"></html>');
-  // response.end('<img src="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg">');
+  console.log(dogUrl) 
+  response.end('<html><p>Hello from Nodejs, where james buys dog tv</p><img src="' + dogUrl + '" alt="dogtv" width="500" height="600"></html>');
 }).listen(3000);

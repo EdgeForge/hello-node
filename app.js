70d507afa0093b757b6659ab51fce9772c9dc75f
Chase McCarthy <chase@code0100fun.com>
2023-05-03 16:46:38 -0500

    Add error handling

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const GIPHY_API_KEY = process.env.GIPHY_API_KEY
 const PORT = Number(process.env.PORT) || 3000
 const QUERY = process.env.QUERY || 'dog'
 
+if(!GIPHY_API_KEY) {
+  throw new Error('GIPHY_API_KEY required')
+}
+
 const http = require('http');
 const path = require('path');
 const fs = require('fs');
@@ -30,17 +34,40 @@ async function getGifs() {
 http.createServer(async(req, res) => {
   const pathname = url.parse(req.url).pathname;
   if (req.method === 'GET' && pathname === '/favicon.ico') {
-    // MIME type of your favicon.
-    //
-    // .ico = 'image/x-icon' or 'image/vnd.microsoft.icon'
-    // .png = 'image/png'
-    // .jpg = 'image/jpeg'
-    // .jpeg = 'image/jpeg'
-    res.setHeader('Content-Type', 'image/png');
-    fs.createReadStream(FAVICON).pipe(res);
+    renderFavicon(res)
+    console.log(`${req.url} - ${res.statusCode}`)
     return;
   }
   const gifs = await getGifs()
+
+  switch (gifs.meta.status) {
+    case 401: renderUnauthorized(res)
+    break;
+    case 200: renderGif(res, gifs)
+    break;
+    default: renderNotFound(res)
+  }
+  console.log(`${req.url} - ${res.statusCode}`)
+}).listen(PORT);
+
+function renderFavicon(res) {
+  // MIME type of your favicon.
+  //
+  // .ico = 'image/x-icon' or 'image/vnd.microsoft.icon'
+  // .png = 'image/png'
+  // .jpg = 'image/jpeg'
+  // .jpeg = 'image/jpeg'
+  res.setHeader('Content-Type', 'image/png');
+  fs.createReadStream(FAVICON).pipe(res);
+}
+
+function renderNotFound(res) {
+  res.statusCode = 404;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`<html><p>No results</p></html>`);
+}
+
+function renderGif(res, gifs) {
   const selected = Math.floor(Math.random() * 50)
   const selectedGif = gifs.data[selected]
   const image = selectedGif.images.original
@@ -49,8 +76,13 @@ http.createServer(async(req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end(`<html><p>Hello from Nodejs and ${QUERY}tv, now with more ${QUERY}s</p><img src="${image.url}" alt="${alt}" width="${width}" height="${height}"></html>`);
-  console.log(`${req.url} ${res.statusCode} - ${image.url}`)
-}).listen(PORT);
+}
+
+function renderUnauthorized(res) {
+  res.statusCode = 401;
+  res.setHeader('Content-Type', 'text/html');
+  res.end(`<html><p>Invalid GIPHY_API_KEY</p></html>`);
+}
 
 function scale(width, height, toWidth) {
   const ratio = toWidth / width
